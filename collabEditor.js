7d6121bd129e7d2cc20f05789ad6f47659e08259
kartikv20 <kartikv20@gmail.com>
2018-04-08 18:51:11 +0530

    Update collabEditor.js

--- collabEditor.js
+++ collabEditor.js
@@ -6,8 +6,8 @@ import fire from './fire'
 import brace from 'brace';
 import AceEditor from 'react-ace';
 
-import 'brace/mode/java';
-import 'brace/theme/github';
+import 'brace/mode/javascript';
+import 'brace/theme/monokai';
 
 
 var database = fire.database();
@@ -24,44 +24,47 @@ constructor(props)
 }
 componentDidMount() {
   database.ref('/collab/code').on('value', snapshot => {
-    const messages = snapshot.val() || {};
+    var messages = snapshot.val() || "";
     console.log({messages});
-    this.setState({newMessage: messages})
+   this.setState({newMessage:messages})
   })
 }
 
-addMessage(new1) {
-  database.ref('/collab/code').set({
-    message:this.state.newMessage
-  })
-  this.setState({
-    newMessage: new1
-  });
-}
+// sendToFirebase(new1) {
+//   database.ref('/collab/code').set({
+//     message:this.state.newMessage
+//   })
+//   this.setState({
+//     newMessage: new1
+//   })
+// }
 
 
 
 
   render() {
+    //console.log(this.state.newMessage)
     return ( <div>
       <AceEditor
-  mode="javascript"
-  theme="monokai"
-  name="blah2"
-  onLoad={this.onLoad}
-  onChange={(new1) => {this.addMessage(new1)}}
-  fontSize={14}
-  showPrintMargin={true}
-  showGutter={true}
-  highlightActiveLine={true}
-  value={this.state.newMessage}
-  setOptions={{
-  enableBasicAutocompletion: false,
-  enableLiveAutocompletion: false,
-  enableSnippets: false,
-  showLineNumbers: true,
-  tabSize: 2,
-  }}/>
+                mode="javascript"
+                theme="monokai"
+                name="blah2"
+                onLoad={this.onLoad}
+                onChange={(new1) => {database.ref("/collab/code").set(new1)}}
+                fontSize={14}
+                showPrintMargin={true}
+                showGutter={true}
+                highlightActiveLine={true}
+                value={this.state.newMessage}
+                height={"1000px"}
+                width={"1200px"}
+                setOptions={{
+                enableBasicAutocompletion: false,
+                enableLiveAutocompletion: false,
+                enableSnippets: false,
+                showLineNumbers: true,
+                tabSize: 2,
+                }}/>
 
         
           
@@ -73,3 +76,4 @@ addMessage(new1) {
 
 
 export default CollabEditor;
+
